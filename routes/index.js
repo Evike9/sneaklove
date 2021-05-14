f9680506d26800fc7b2a5cc72259392fe8c100e9
Eva Evike <evikeprog@gmail.com>
2021-05-14 11:00:07 +0200

    v1

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-return console.log(`\n\n
------------------------------
------------------------------
-     wax on / wax off !
------------------------------
------------------------------\n\n`
-);
+// added : 
+// const sneakerModel = require("./../model/Sneaker");
+// const tagModel = require("./../model/Tag");
+// const userModel = require("./../model/User");
+
+
+
 
 router.get("/", (req, res) => {
-  res.send("foo");
+  res.render("index.hbs");
 });
 
-router.get("/sneakers/:cat", (req, res) => {
-  res.send("bar");
+router.get("/sneakers/:id", (req, res) => {
+  res.render("partial/sneaker_mini.hbs");
 });
 
 router.get("/one-product/:id", (req, res) => {
